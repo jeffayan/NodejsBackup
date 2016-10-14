400de1cfdf57955028d2c45032e89eeb4cd713c6
jeffayan <jeffayangdr@gmail.com>
2016-10-14 13:37:00 +0530

    Incomplete - task : New Task

--- hapi.js
+++ hapi.js
@@ -1,34 +1,31 @@
 'use strict';
 'use esversion: 6';
-const bcrypt=require('bcrypt');
-const basic = require('hapi-auth-basic');
+
 const hapi = require("hapi");
 const inert=require('inert');
 const good= require('good');
-const server = new hapi.Server();
+var delayed=require('delayed');
+const server = new hapi.Server({
+	cache:[{
+		name:'mongoCache',
+		engine:require('catbox-mongodb'),
+		host:'172.16.19.120',
+		partition:'cache'
+	}]
+	
+});
+const add = function (a,b,next){
+	return next(null,Number(a)+Number(b));
+}
 
+const sumCache = server.cache({
+	cache:'mongoCache',
+	expiresIn:1000,
+    segment:'customSegment',
+    generateFunc: function (id,next){add(id.a,id.b,next);},
+    generateTimeout: 100
+});
 server.connection({port:'8000'});
-
-const users={
-		jeff:{
-			username:'jeff',
-			password:'jeff*123',
-			name:'jeffAyan'
-		}
-};
-
-const validate = function(request,username,password,callback){
-	const user = users[username];
-	if(!user)
-		{
-			callback(null,false);
-		}
-	bcrypt.compare(password,user.password,(err,isValid) => {
-		callback(err,isValid,{name:user.name});
-	});
-	
-	
-};
 
 server.register([{
 	register:good,
@@ -51,24 +48,36 @@ server.register([{
 	}
 },{
 	register:inert
-},{
-    register:basic	
 }],err=>{
 	if(err) 
 		throw err; 
-	server.auth.strategy("simple", "basic",{validateFunc:validate});
+	
 server.route({
 	
 	method:"get",
-	path:'/',
+	path:'/{a}/{b}',
+	handler: function (req,res){
+		
+		const id = req.params.a+':'+req.params.b;
+		sumCache.get({id:id,a:req.params.a,b:req.params.b},(err,result)=>{
+			if(err) return res(err);
+			else {res(result);}
+		});
+		//const response = res({ be: 'hapi' });
+		//console.log("edd "+req.params.ttl);
+	//	var name="";
+	    //function hh(){ for(var i=0;i<150000;i++){console.log(i); name="jeff";}}
+	   /* hh();
+		console.log("name "+JSON.stringify(req.params.namee));
+		
+		return res(name).header('mod',Date.toUTCString);*/
+	},
 	config:{
-		  auth:'simple',
-		  handler: function (req,res){
-		 console.warn("Hello"+req.auth.credentials.name);
-		return res("Hello"+req.auth.credentials.name);
+		cache:{
+			expiresIn:5000,
+			privacy:'private'
+		}
 	}
-	}
-	
 				
 });
 });
--- newHapi.js
+++ newHapi.js
@@ -1,10 +1,62 @@
-var Notify = window.Notify.default;
+/*var fullText ="hello";
+var g ="hel";
 
-var myNotification = new Notify('Yo dawg!', {
-    body: 'This is an awesome notification',
-    notifyShow: onNotifyShow
+fullText=fullText.replace(g,g+',').split(',');
+
+console.log(fullText);*/
+
+
+/*var fullText ="hellohello";
+var givenCharacters ="hel";
+var output = fullText.split( givenCharacters ).map( function(item, index, arr){ 
+	console.log("item:"+item+"  index:"+index+"   arr:"+arr);
+    if ( index == arr.length - 1 ) 
+    { 
+      return item; 
+    } 
+    else
+    { 
+      return item + givenCharacters  
+    } 
+});AIzaSyBP4rAhmyVSoH9sDfwhUPssNSlz6jX9ptM
+
+
+
+console.log( output );*/
+
+
+
+var timezone = require('node-google-timezone');
+ 
+var timestamp = 1402629305; 
+// time as seconds since midnight, January 1, 1970 UTC 
+ 
+console.log(new Date(timestamp * 1000));
+// => Fri Jun 13 2014 00:15:05 GMT-0300 (BRT) 
+ 
+// somewhere in New York 
+var lat = 15.650407, 
+    lng = 101.672417;
+ 
+//timezone.key('YOUR API KEY HERE'); // optional 
+ 
+//timezone.language('es'); // optional: default 'en' 
+ 
+timezone.data(lat, lng, timestamp, function (err, tz) {
+ 
+  console.log(tz.raw_response);
+  //=> { dstOffset: 3600, 
+  //     rawOffset: -18000, 
+  //     status: 'OK', 
+  //     timeZoneId: 'America/New_York', 
+  //     timeZoneName: 'Eastern Daylight Time' } 
+ 
+  console.log(tz.local_timestamp);
+  // => 1402614905 
+ 
+  var d = new Date(tz.local_timestamp * 1000);
+ 
+  console.log(d.toDateString() + ' - ' + d.getHours() + ':' + d.getMinutes());
+  // => Thu Jun 12 2014 - 20:15 
+ 
 });
-
-function onNotifyShow() {
-    console.log('notification was shown!');
-}
